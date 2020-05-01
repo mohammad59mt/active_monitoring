4efca2b39186b7151b6df7d4897c65eaba3b635e
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2020-05-02 01:46:44 +0430

    run test without mininet using a simple bash script

---
+++ run_no_mininet.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+
+#run this code like ./run_no_mininet.sh <path-to-last-run>
+#don't forget to make this file executable, chmod +x run_no_mininet.sh
+if [ -z "$1" ]
+then
+      echo "Error: Please enter last run path"
+      exit 2
+fi
+python3 program.py --pb --pop 50 --iter 50 --no-mininet --toponame abilene --last-result-path $1 --n 1 --new
+python3 program.py --pb --pop 50 --iter 50 --no-mininet --toponame abilene --last-result-path $1 --n 5
+python3 program.py --pb --pop 50 --iter 50 --no-mininet --toponame abilene --last-result-path $1 --n 5
+python3 program.py --pb --pop 50 --iter 50 --no-mininet --toponame abilene --last-result-path $1 --n 5
+python3 program.py --pb --pop 50 --iter 50 --no-mininet --toponame abilene --last-result-path $1 --n 5
